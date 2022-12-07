70d58a01c4a3ed45ee216e9eb635f26a518ce73c
Dov_Dembin <Dov.Dembin@Dell.com>
2022-12-07 18:57:55 +0200

    dd

--- vars/getVersionSuffix.groovy
+++ vars/getVersionSuffix.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
     node {
         if ("${config.isReleaseCandidate}") {
-            return "${config.rcNumber}"
+            return config.rcNumber
         } else {
             return "${config.rcNumber}" + '+ci.' + "${env.BUILD_NUMBER}"
         }
